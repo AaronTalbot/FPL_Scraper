3f8f8fd7e509210ecf757e3fcea6e391ecc67274
Aaron Talbot <aaron.talbot@mycit.ie>
2022-08-04 15:41:18 +0100

    player update implemented

--- Players.py
+++ Players.py
@@ -8,10 +8,13 @@ async def main():
         players = await fpl.get_players(return_json=True)
         for player in players:
             file_name = str(player["first_name"]) + str(player["second_name"])+str(player["code"])
-            file_name = file_name.replace(" ","")
-            print(player)
+            file_name = file_name.replace(" ", "")
+            csvdf = pd.read_excel("Players/"+file_name+".xlsx")
             df = pd.DataFrame([player])
-            df.to_csv("Players/"+file_name)
+
+            vertical_concat = pd.concat([csvdf, df], axis=0)
+            vertical_concat.to_excel("Players/"+file_name+".xlsx")
+            # df.to_excel("Players/" + file_name+".xlsx")
 
 
 if __name__ == "__main__":
